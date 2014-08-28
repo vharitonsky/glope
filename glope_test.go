f4aaef9ce7d5349e961370904590bd920babcbd1
Vitaly Haritonsky <vharitonsky@gmail.com>
2014-08-28 23:07:32 +0300

    added profit calculation testing

--- glope_test.go
+++ glope_test.go
@@ -1,9 +1,22 @@
 package glope
 
 import (
+	"math"
 	"testing"
 )
 
 func TestClusterStruct(t *testing.T) {
 	//stub
+}
+
+func TestGetProfit(t *testing.T) {
+	s := 5
+	w := 3
+	r := 4.0
+	expectedProfit := float64(s) / math.Pow(float64(w), r)
+	calculatedProfit := getProfit(s, w, r)
+	if expectedProfit != calculatedProfit {
+		t.Error("Expected profit was ", expectedProfit, " got ", calculatedProfit)
+		t.FailNow()
+	}
 }
